7f502e8cfe40e0a75fa00938dc899a2936252ea2
oZone <positronix10@gmail.com>
2019-11-12 20:49:50 +0100

    Update versioning

--- Plugin/version.h
+++ Plugin/version.h
@@ -1,9 +1,16 @@
-#define PLUGIN_VERSION "0.1.2.1"
-#define PLUGIN_AUTHOR "oZone"
-#define PLUGIN_YEAR 2019
+#pragma once
 // Has to be the same as the project name!
-#define PLUGIN_NAME "TranslucentRM"
+#define PLUGIN_NAME             "TranslucentRM"
+#define PLUGIN_VER_MAJOR        1
+#define PLUGIN_VER_MINOR        0
+#define PLUGIN_VER_REVISION     0
+#define PLUGIN_VER_BUILD        8
+#define PLUGIN_AUTHOR           "oZone"
+#define PLUGIN_YEAR             2019
+#define RAINMETER_VER_MAJOR     4
+#define RAINMETER_VER_MINOR     4
+#define RAINMETER_VER_REVISION  0
+#define RAINMETER_VER_BUILD     3330
 // This is the path relative to root directory (where build.bat is placed)
 //#define EXAMPLE_SKIN_PATH "ExampleSkin"
 //#define EXAMPLE_SKIN_NAME "Example Skin"
-#define RAINMETER_VERSION "4.0.0.0"
