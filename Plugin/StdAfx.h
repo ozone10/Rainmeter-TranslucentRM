76e55be98f89fd0f9b0d95c178cc0be73cba14cb
oZone <positronix10@gmail.com>
2020-04-19 12:51:52 +0200

    Update files

--- Plugin/StdAfx.h
+++ Plugin/StdAfx.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2019 oZone
+  Copyright (C) 2019-2020 oZone
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
--- Plugin/version.h
+++ Plugin/version.h
@@ -6,11 +6,11 @@
 #define PLUGIN_VER_REVISION     3
 #define PLUGIN_VER_BUILD        0
 #define PLUGIN_AUTHOR           "oZone"
-#define PLUGIN_YEAR             2019
+#define PLUGIN_YEAR             2020
 #define RAINMETER_VER_MAJOR     4
 #define RAINMETER_VER_MINOR     4
 #define RAINMETER_VER_REVISION  0
-#define RAINMETER_VER_BUILD     3330
+#define RAINMETER_VER_BUILD     3348
 // This is the path relative to root directory (where build.bat is placed)
 //#define EXAMPLE_SKIN_PATH "ExampleSkin"
 //#define EXAMPLE_SKIN_NAME "Example Skin"
