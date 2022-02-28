183e29281dc986e7a844ad077c04c5cd51cc1840
ozone10 <positronix10@gmail.com>
2022-02-28 19:01:32 +0100

    Update build tools

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-@echo Choose Visual Studio 2019 Edition (after 5s will default to (2) Enterprise):
+@echo Choose Visual Studio 2022 Edition (after 5s will default to (2) Enterprise):
 ::https://help.github.com/en/actions/automating-your-workflow-with-github-actions/software-installed-on-github-hosted-runners#visual-studio-2019-enterprise
 ::This is because I am poor.
 :Reset
@@ -17,7 +17,7 @@ goto Continue
 set Edition=Community
 goto Continue
 :Continue
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\%Edition%\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "C:\Program Files\Microsoft Visual Studio\2022\%Edition%\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 set solutionsFile=".\SDK-CPP.sln"
 msbuild -m /t:build /p:Configuration=Release;Platform=x64 %solutionsFile%
 msbuild -m /t:build /p:Configuration=Release;Platform=Win32 %solutionsFile%
